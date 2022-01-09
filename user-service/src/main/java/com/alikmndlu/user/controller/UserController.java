027b0b0d8e17336423690d1876568699445af8a7
Ali Kmndlu <alikmndlu1@gmail.com>
2022-01-09 13:06:31 +0330

    add valid jwt token required for getUserWithAddresses api

--- user-service/src/main/java/com/alikmndlu/user/controller/UserController.java
+++ user-service/src/main/java/com/alikmndlu/user/controller/UserController.java
@@ -4,6 +4,11 @@ import com.alikmndlu.user.dto.UserAddressesListDto;
 import com.alikmndlu.user.dto.UserCredentialsDto;
 import com.alikmndlu.user.model.User;
 import com.alikmndlu.user.service.UserService;
+import com.auth0.jwt.JWT;
+import com.auth0.jwt.JWTVerifier;
+import com.auth0.jwt.algorithms.Algorithm;
+import com.auth0.jwt.exceptions.JWTVerificationException;
+import com.auth0.jwt.interfaces.DecodedJWT;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.ResponseEntity;
@@ -42,7 +47,23 @@ public class UserController {
 
     // Get User With Addresses List
     @GetMapping("/{id}")
-    public ResponseEntity<UserAddressesListDto> getUserWithAddresses(@PathVariable Long id) {
+    public ResponseEntity<?> getUserWithAddresses(
+            @PathVariable Long id,
+            @RequestHeader(name = "Authorization", required = false) String token) {
+        log.info("Inside getUserWithAddresses method of UserController");
+
+        if (token == null)
+            return ResponseEntity.badRequest().build();
+
+        try {
+            JWTVerifier verifier = JWT.require(Algorithm.HMAC256("secret-key")).withIssuer("auth0").build();
+            DecodedJWT jwt = verifier.verify(token);
+            log.info("Valid Token Sent To The Server, {}", jwt.getSubject());
+        } catch (JWTVerificationException exception){
+            log.info("Invalid Token Sent To The Server");
+            return ResponseEntity.badRequest().build();
+        }
+
         log.info("Inside getUserWithAddresses method of UserController");
         return ResponseEntity.ok().body(userService.getUserWithAddresses(id));
     }
